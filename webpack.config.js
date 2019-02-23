9e1cbb88ca21d307393a895884f9c06880869607
Alexander Andreev <alexander.andreev@restream.rt.ru>
2019-02-23 11:10:56 +0400

    add test

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,6 @@
-const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.jsx',
-  output: {
-    path: path.join(__dirname, '/dist'),
-    filename: 'index_bundle.js',
-  },
   module: {
     rules: [
       {
